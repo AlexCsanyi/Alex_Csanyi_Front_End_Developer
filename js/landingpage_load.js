db9483f1336c4212dd2a6ac127b2ed9296ddf641
AlexCsanyi <alex.csanyi@gmail.com>
2019-07-29 06:44:51 +0100

    added animations

---
+++ js/landingpage_load.js
@@ -0,0 +1,17 @@
+TweenMax.from("#brand", 1, {
+  delay: 0.4,
+  y: 10,
+  opacity: 0,
+  ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(
+  "#menu li a",
+  1,
+  {
+    delay: 0.4,
+    opacity: 0,
+    ease: Expo.easeInOut
+  },
+  0.1
+);
