b41c1e4ec666e85bf322a97d48e3e11cef0fdc52
AlexCsanyi <alex.csanyi@gmail.com>
2019-09-08 19:29:27 +0100

    fixed fetching - page now is animated instead of wow.js - more responsive this way - images also reload

--- js/fetch.js
+++ js/fetch.js
@@ -36,13 +36,15 @@ function fetchPage(link, page) {
       let doc = new DOMParser().parseFromString(html, "text/html");
 
       anime({
-        targets: ".hero-content h1, .hero-content p, .hero-content h6",
+        targets:
+          ".hero-content h1, .hero-content p, .hero-content h6, .image-section .container",
         translateY: -700,
         opacity: 0,
-        easing: "easeInExpo",
+        easing: "linear",
         duration: 700,
         complete: anim => {
           document.querySelector(".hero-wrapper").remove();
+          document.querySelector(".image-section").remove();
         }
       });
 
@@ -51,8 +53,31 @@ function fetchPage(link, page) {
           .querySelector(".wrapper")
           .insertBefore(
             doc.querySelector(".new-content"),
-            document.querySelector(".image-section")
+            document.querySelector(".insert-desc-here")
           );
+        anime({
+          targets: ".new-content",
+          translateY: [700, 0],
+          delay: (el, i) => 100 * i,
+          opacity: [0, 1],
+          easing: "linear"
+        });
+      }, 300);
+
+      setTimeout(function() {
+        document
+          .querySelector(".wrapper")
+          .insertBefore(
+            doc.querySelector(".new-img-content"),
+            document.querySelector(".insert-img-here")
+          );
+        anime({
+          targets: ".new-img-content .container",
+          translateY: [700, 0],
+          delay: (el, i) => 100 * i,
+          opacity: [0, 1],
+          easing: "linear"
+        });
       }, 300);
     });
 }
