188bdd4f26ab42ea9ba406ad928288fba4fcc75d
AlexCsanyi <alex.csanyi@gmail.com>
2024-02-03 15:38:52 +0000

    eslint and fix errors

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const main = document.getElementById('main');
 const containerSuccess = document.getElementById('success');
 const containerError = document.getElementById('error');
+const spanErrorMessage = document.getElementById('error-message');
 
 const form = document.getElementById('contact-form');
 const containerBtnSubmit = document.getElementById('container-btn-submit');
@@ -37,6 +38,8 @@ const handleSubmit = (event) => {
     })
     .catch((error) => {
       // show the error message
+      console.error(error);
+      spanErrorMessage.textContent = error.message;
       window.scrollTo({ top: 0, behavior: 'smooth' });
       main.style.display = 'none';
       containerError.style.display = 'block';
@@ -67,28 +70,25 @@ btnsBack.forEach((el) => el.addEventListener('click', handleBack));
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let windowHeight;
-let scrolled;
-let docHeight;
-let scrollPer;
-
 function render() {
-  windowHeight = window.innerHeight;
-  (scrolled = window.pageYOffset),
-    (docHeight = Math.max(
-      document.documentElement.clientHeight,
-      document.documentElement.scrollHeight,
-      document.documentElement.offsetHeight,
-      document.body.scrollHeight,
-      document.body.offsetHeight
-    )),
-    (scrollPer = scrolled / (docHeight - windowHeight));
+  const windowHeight = window.innerHeight;
+  const scrolled = window.scrollY;
+  const docHeight = Math.max(
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight,
+    document.body.scrollHeight,
+    document.body.offsetHeight
+  );
+
+  const scrollPercentage = scrolled / (docHeight - windowHeight);
 
   ctx.clearRect(0, 0, 25, 25);
   ctx.lineWidth = 5;
   makeArc('#666666', 12.5, 12.5, 10, 2 * Math.PI);
-  makeArc('#FFFFFF', 12.5, 12.5, 10, scrollPer * 2 * Math.PI);
+  makeArc('#FFFFFF', 12.5, 12.5, 10, scrollPercentage * 2 * Math.PI);
 }
+
 function makeArc(color, one, two, three, five) {
   ctx.beginPath();
   ctx.strokeStyle = color;
@@ -99,15 +99,17 @@ function makeArc(color, one, two, three, five) {
 window.onresize = function () {
   render();
 };
+
 window.onscroll = function () {
   render();
 };
+
 render();
 
 // cta btn cursor animation
 const cursor = document.querySelector('#cursor');
-let mouse = { x: -100, y: -100 };
-let cursorPosition = { x: 0, y: 0 };
+const mouse = { x: -100, y: -100 };
+const cursorPosition = { x: 0, y: 0 };
 const cursorSpeed = 0.1;
 
 const updateCursorCoordinates = (event) => {
@@ -162,84 +164,78 @@ const calcBoundingClientRect = (domElement) => {
   };
 };
 
-let mouseMagneticPosition = { x: 0, y: 0 };
+const mouseMagneticPosition = { x: 0, y: 0 };
 window.addEventListener('mousemove', (e) => {
   mouseMagneticPosition.x = e.pageX;
   mouseMagneticPosition.y = e.pageY;
 });
 
-class MagneticObject {
-  constructor(domElement) {
-    this.domElement = domElement;
-    this.boundingClientRect = calcBoundingClientRect(this.domElement);
+const makeMagnetic = (domElement) => {
+  let boundingClientRect = calcBoundingClientRect(domElement);
+  const triggerArea = 200;
+  const interPolationFactor = 0.8;
+  const lerpingData = {
+    x: { current: 0, target: 0 },
+    y: { current: 0, target: 0 },
+  };
 
-    this.triggerArea = 200; // 200px
-    this.interPolationFactor = 0.8;
+  const resize = () => {
+    boundingClientRect = calcBoundingClientRect(domElement);
+  };
 
-    this.lerpingData = {
-      x: { current: 0, target: 0 },
-      y: { current: 0, target: 0 },
-    };
-
-    this.resize();
-    this.render();
-  }
-
-  resize() {
-    window.addEventListener('resize', (e) => {
-      this.boundingClientRect = calcBoundingClientRect(this.domElement);
-    });
-  }
-
-  render() {
-    const boundclientRectX =
-      this.boundingClientRect.left + this.boundingClientRect.width / 2;
-    const boundclientRectY =
-      this.boundingClientRect.top + this.boundingClientRect.height / 2;
+  const render = () => {
+    const centerX = boundingClientRect.left + boundingClientRect.width / 2;
+    const centerY = boundingClientRect.top + boundingClientRect.height / 2;
 
     const distanceFromMouseToCenter = calculateDistance(
       mouseMagneticPosition.x,
       mouseMagneticPosition.y,
-      boundclientRectX,
-      boundclientRectY
+      centerX,
+      centerY
     );
 
-    let targetHolder = { x: 0, y: 0 };
-    if (distanceFromMouseToCenter < this.triggerArea) {
-      targetHolder.x = (mouseMagneticPosition.x - boundclientRectX) * 0.3;
-      targetHolder.y = (mouseMagneticPosition.y - boundclientRectY) * 0.3;
+    if (distanceFromMouseToCenter < triggerArea) {
+      lerpingData.x.target = (mouseMagneticPosition.x - centerX) * 0.3;
+      lerpingData.y.target = (mouseMagneticPosition.y - centerY) * 0.3;
+    } else {
+      lerpingData.x.target = 0;
+      lerpingData.y.target = 0;
     }
 
-    this.lerpingData['x'].target = targetHolder.x;
-    this.lerpingData['y'].target = targetHolder.y;
+    lerpingData.x.current = lerp(
+      lerpingData.x.current,
+      lerpingData.x.target,
+      interPolationFactor
+    );
 
-    for (const coord in this.lerpingData) {
-      this.lerpingData[coord].current = lerp(
-        this.lerpingData[coord].current,
-        this.lerpingData[coord].target,
-        this.interPolationFactor
-      );
-    }
+    lerpingData.y.current = lerp(
+      lerpingData.y.current,
+      lerpingData.y.target,
+      interPolationFactor
+    );
 
-    this.domElement.style.transform = `translate(${this.lerpingData['x'].current}px,${this.lerpingData['y'].current}px)`;
+    domElement.style.transform = `translate(${lerpingData.x.current}px, ${lerpingData.y.current}px)`;
 
-    window.requestAnimationFrame(() => this.render());
-  }
-}
+    window.requestAnimationFrame(render);
+  };
+
+  window.addEventListener('resize', resize);
+  render();
+};
 
 const magneticButton = document.querySelector('#container-btn-submit');
-new MagneticObject(magneticButton);
+makeMagnetic(magneticButton);
 
 // scrollTrigger
 function addObserver(el, options) {
   // handle unsupported browsers
   if (!('IntersectionObserver' in window)) {
     // The animation will be called immediately so
-    entry.target.classList.add('active');
+    el.classList.add('active');
     return;
   }
 
-  let observer = new IntersectionObserver((entries, observer) => {
+  const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
       // `entry.isIntersecting` will be true if the element is visible
       if (entry.isIntersecting) {
